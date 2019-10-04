57e99730c75a288cf69ad7e54cefb297c01f42ac
小蓝拳拳 <55010695+githuber-007@users.noreply.github.com>
2019-10-04 18:25:47 +0800

    Update seven_addr.java

--- seven_addr.java
+++ seven_addr.java
@@ -49,7 +49,7 @@ protected void writeinto(String output)
 		out.write(c1+this.fifth_addr+c2);
 		out.write(c1+this.sixth_addr+c2+",");
 		out.write(c1+this.seventh_addr+c2);
-		out.write("]},"+"\r\n");
+		out.write("]},");
 		out.close();
 	}
 	catch(Exception e)
